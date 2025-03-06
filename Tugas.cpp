e7be5fb25592e46c64d831bb0fd7310039e5b333
Crazy0nes <ajax9370@gmail.com>
2025-03-07 06:36:43 +0700

    Merapikan Kode, Penambahan Fungsi, dan Finalisasi

--- Tugas.cpp
+++ Tugas.cpp
@@ -2,53 +2,168 @@
 #include <iomanip>
 using namespace std;
 
-int main(){
+int index_terbesar=0;
 
-    struct Lagu{
-        string judulLagu;
-        string penyanyi;
-        string genre;
-        int tahun;
-    };Lagu song[100];
+
+struct Lagu{
+    string judulLagu;
+    string penyanyi;
+    string genre;
+    int tahun;
+};Lagu song[100];
+
+void quicksort(Lagu array[], int low, int high) {
+    int awal = low;
+    int akhir = high;
+    string pivot = array[(awal + akhir) / 2].judulLagu;
+    do {
+        while (array[awal].judulLagu < pivot) {
+            awal++;
+        }
+        while (array[akhir].judulLagu > pivot) {
+            akhir--;
+        }
+        if (awal <= akhir) {
+            swap(array[awal], array[akhir]);
+            awal++;
+            akhir--;
+        }
+    } while (awal <= akhir);
+
+    if (low < akhir) {
+        quicksort(array, low, akhir);
+    }
+    if (awal < high) {
+        quicksort(array, awal, high);
+    }
+}
+
+void tambah() {
+	int jumlahInput = 0;
+	while (true)
+	{
+		cout << "Masukkan jumlah lagu untuk diinput : ";
+		cin >> jumlahInput;
+
+		if (jumlahInput > 0) {
+			break;
+		}
+		else {
+			cout << "jumlah input tidak valid, ulangi!" << endl;
+		}
+	}
+
+	string judul, penyanyi, genre;
+	int tahun;
+	Lagu temp;
+
+	for (int i = 0; i < jumlahInput; i++)
+	{   cout << "Input ke - " << i + 1 << endl;
+		cout << "Masukan judul		: ";
+		cin.ignore();
+		getline(cin, judul);
+		cout << "Masukan penyanyi	: ";
+		getline(cin, penyanyi);
+		cout << "Masukan genre		: ";
+		getline(cin, genre);
+		cout << "Masukan tahun		: ";
+		cin >> tahun;
+
+		temp = { judul, penyanyi, genre, tahun };
+
+		song[index_terbesar] = temp;
+		index_terbesar++;
+	}
+	cout << endl;
+
+	quicksort(song, 0, index_terbesar - 1);
+}
+
+int binary_search(string pencarian) {
+	int index_awal = 0;
+	int index_akhir = index_terbesar;
+	bool search = false;
+	while (search == false && index_awal <= index_akhir) {
+		int mid = (index_awal + index_akhir) / 2;
+		if (song[mid].judulLagu == pencarian) {
+			cout << "Data ditemukan!" << endl;
+			cout << "	judul		: " << song[mid].judulLagu << endl;
+			cout << "	penyanyi	: " << song[mid].penyanyi << endl;
+			cout << "	genre		: " << song[mid].genre << endl;
+			cout << "	tahun		: " << song[mid].tahun << endl;
+			cout << endl;
+			return mid;
+		}
+		else
+		{
+			if (pencarian < song[mid].judulLagu) {
+				index_akhir = mid - 1;
+			}
+			else {
+				index_awal = mid + 1;
+			}
+		}
+	}
+	cout << "Data tidak ditemukan" << endl;
+	return -1;
+
+}
+
+void cari() {
+    bool kondisi=true;
+    char pilihan;
+    string pencarian;
+
+    while(kondisi){
+        cout << "Masukkan judul lagu yang ingin dicari : ";
+        cin.ignore();
+        getline(cin, pencarian);
+        binary_search(pencarian);
+        cout << "Apakah ingin mencari lagi? (y/n) : ";
+        cin >> pilihan;
+        if(pilihan == 'n'){
+            kondisi = false;
+            cout << "\n\n";
+        }
+    }
+}
+
+
+int main(){
+    bool again = true;
+    string lagunya;
+    int jumlahLagu = 0; 
+    do{
 
     cout << "Playlist Lagu" << endl;
     cout << setfill('=') << setw(15) << "=" << endl;
+
+    for (int i = 0; i < index_terbesar; i++)
+	{
+		cout << i + 1 << ". " << song[i].judulLagu << " - " << song[i].penyanyi << "(" << song[i].tahun << ")" << endl;
+	}
+
+    cout << setfill('=') << setw(15) << "=" << endl;
     cout << setfill(' ') << setw(3) << "1. " << "Tambah lagu" << endl;
     cout << setfill(' ') << setw(3) << "2. " << "Cari Lagu" << endl;
-    cout << setfill(' ') << setw(3) << "3. " << "Keluar" << endl;
+    cout << setfill(' ') << setw(3) << "3. " << "Keluar" << endl << endl;
     cout << "Pilih Menu: ";
     int menu;
     cin >> menu;
-    
-    do{
+
+
     switch (menu){
         case 1:
-            cout << "Tambah lagu" << endl;
-            cout << "Masukkan jumlah lagu yang ingin ditambahkan: ";
-            int jumlahLagu;
-            cin >> jumlahLagu;
-            for(int i = 0; i < jumlahLagu; i++){
-                cout << "Masukkan judul Lagu: ";
-                cin >> song[i].judulLagu;
-                cout << "Masukkan penyanyi: ";
-                cin >> song[i].penyanyi;
-                cout << "Masukkan genre: ";
-                cin >> song[i].genre;
-                cout << "Masukkan tahun: ";
-                cin >> song[i].tahun ; cout << endl << endl; 
-            }
+            tambah();
             break;
 
         case 2:
-            cout << "Cari lagu" << endl;
-            cout << "Masukkan judul lagu yang ingin dicari: ";
-            string lagunya;
-            cin >> lagunya;
-
+            cari();
             break;
 
         case 3:
-            cout << "Keluar" << endl;
+            cout << endl << "Keluar dari Program" << endl;
+            again = false; 
             break;
 
         default:
@@ -57,6 +172,6 @@ int main(){
 
         }
         
-    }while(menu != 3);
+    }while( again); 
     return 0;
 }
